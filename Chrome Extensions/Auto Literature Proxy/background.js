48954b054063dd2f144d9183de2f938a344fc6b9
Kyle King <kmking72@gmail.com>
2015-06-21 14:30:37 -0400

    Possibly working!

--- Chrome Extensions/Auto Literature Proxy/background.js
+++ Chrome Extensions/Auto Literature Proxy/background.js
@@ -1,36 +1,38 @@
-// // The code that is fired upon page load
-// jQuery(function($) {
-//   // get currnet url
-//   var str =  window.location.href;
-//   console.log("window.location.href = " + str);
-//
-//   // Hard code the proxy link
-//   var proxyLink = '.proxy-um.researchport.umd.edu';
-//
-//   // Example string split function with clean output
-//   // Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
-//   function splitString(stringToSplit, separator) {
-//     var arrayOfStrings = stringToSplit.split(separator);
-//
-//     console.log('The original string is: "' + stringToSplit + '"');
-//
-//     // Catch error case
-//     if (arrayOfStrings.length != 2){
-//       console.log('Split error, there are ' + arrayOfStrings.length + ' elements');
-//     }
-//
-//     // Request redirect to split and joined url
-//     var redirectUrl = arrayOfStrings.join(separator + proxyLink);
-//     console.log('The array has ' + arrayOfStrings.length + ' elements: ' + redirectUrl);
-//     chrome.extension.sendRequest({redirect: redirectUrl});
-//
-//     chrome.extension.onRequest.addListener(function(request, sender) {
-//         chrome.tabs.update(sender.tab.id, {url: request.redirect});
-//     });
-//   }
-//
-//   // The goal:
-//   // http://pubs.rsc.org.proxy-um.researchport.umd.edu/en/Content/ArticleLanding/2007/LC/b712893m#!divAbstract
-//   // http://pubs.rsc.org/en/content/articlelanding/2007/lc/b712893m#!divAbstract
-//   splitString(str, '.org');
-// });
+// The code that is fired upon page load
+jQuery(function($) {
+  // get currnet url
+  var str =  window.location.href;
+  console.log("window.location.href = " + str);
+
+  // Hard code the proxy link
+  var proxyLink = '.proxy-um.researchport.umd.edu';
+
+  // Example string split function with clean output
+  // Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+  function splitString(stringToSplit, separator) {
+    var arrayOfStrings = stringToSplit.split(separator);
+
+    console.log('The original string is: "' + stringToSplit + '"');
+
+    // Catch error case
+    if (arrayOfStrings.length != 2){
+      console.log('Split error, there are ' + arrayOfStrings.length + ' elements');
+      return 'Error';
+    }
+
+    // Request redirect to split and joined url
+    var redirectUrl = arrayOfStrings.join(separator + proxyLink);
+    console.log('The array has ' + arrayOfStrings.length + ' elements: ' + redirectUrl);
+
+    chrome.extension.sendRequest({redirect: redirectUrl});
+    // Controllable test
+    // chrome.extension.sendRequest({redirect: 'http://www.google.com'});
+    console.log('Request Sent');
+  }
+
+  // The goal:
+  // http://pubs.rsc.org.proxy-um.researchport.umd.edu/en/Content/ArticleLanding/2007/LC/b712893m#!divAbstract
+  // http://pubs.rsc.org/en/content/articlelanding/2007/lc/b712893m#!divAbstract
+  // Need check for when the site has redirected to stop redirecting because the links are identical
+  splitString(str, '.org');
+});

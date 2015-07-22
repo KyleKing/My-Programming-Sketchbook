2be544b8cffba083090ceedc51af0b500a959866
Kyle King <kmking72@gmail.com>
2015-07-22 08:04:20 -0400

    Added observe query

--- ENGL393/BikeShare/BikeShare.js
+++ ENGL393/BikeShare/BikeShare.js
@@ -23,7 +23,7 @@ if (Meteor.isServer) {
   // Insert data into the database
   if (DailyBikeData.find().count() === 0) {
     var i = 1;
-    while (i <= 50) {
+    while (i <= 2) {
       var randCoordinates = randGPS();
       DailyBikeData.insert({
         Bike: i,
@@ -58,66 +58,52 @@ if (Meteor.isClient) {
     L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
     var map = new L.Map('BikeMap', { center: new L.LatLng(38.987701, -76.940989) });
     L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
-    map.spin(true);
+    map.spin(false);
 
-    // Receive data from server and display on map and rerun on server updates
-    Meteor.autorun(function() {
-      var bikesData = DailyBikeData.find().fetch();
-      bikesData.forEach(function(bike) {
-        var latlng = [bike.Positions.lat, bike.Positions.lng];
-        var marker = L.marker(latlng).addTo(map);
-        marker.bindPopup("#" + bike.Bike + " is " + bike.Tag);
-      });
-    });
-
-
-    // // // Source: http://meteorcapture.com/how-to-create-a-reactive-google-map/
-    // DailyBikeData.find().observe({
-    //   added: function(document) {
-    //     console.log(document);
-    //     // var latlng = [bike.Positions.lat, bike.Positions.lng];
-    //     // var marker = L.marker(latlng).addTo(map);
-    //     // marker.bindPopup("#" + bike.Bike + " is " + bike.Tag);
-
-
-    //     // // Create a marker for this document
-    //     // var marker = new google.maps.Marker({
-    //     //   draggable: true,
-    //     //   animation: google.maps.Animation.DROP,
-    //     //   position: new google.maps.LatLng(document.lat, document.lng),
-    //     //   map: map.instance,
-    //     //   // We store the document _id on the marker in order
-    //     //   // to update the document within the 'dragend' event below.
-    //     //   id: document._id
-    //     // });
-
-    //     // // This listener lets us drag markers on the map and update their corresponding document.
-    //     // google.maps.event.addListener(marker, 'dragend', function(event) {
-    //     //   Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
-    //     // });
-
-    //     // // Store this marker instance within the markers object.
-    //     // markers[document._id] = marker;
-    //   },
-    //   // // changed: function(newDocument, oldDocument) {
-    //   // //   markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
-    //   // // },
-    //   // removed: function(oldDocument) {
-    //   //   // Remove the marker from the map
-    //   //   markers[oldDocument._id].setMap(null);
-
-    //   //   // Clear the event listener
-    //   //   google.maps.event.clearInstanceListeners(
-    //   //     markers[oldDocument._id]);
-
-    //   //   // Remove the reference to this marker instance
-    //   //   delete markers[oldDocument._id];
-    //   // }
+    // // Receive data from server and display on map and rerun on server updates
+    // Meteor.autorun(function() {
+    //   var bikesData = DailyBikeData.find().fetch();
+    //   bikesData.forEach(function(bike) {
+    //     var latlng = [bike.Positions.lat, bike.Positions.lng];
+    //     var marker = L.marker(latlng).addTo(map);
+    //     marker.bindPopup("#" + bike.Bike + " is " + bike.Tag);
+    //   });
     // });
-    // // Similar Alt: http://asynchrotron.com/blog/2013/12/28/realtime-maps-with-meteor-and-leaflet-part-2/
-    // //
-    // // Packages? https://atmospherejs.com/?q=leaflet
 
+    // Source: http://meteorcapture.com/how-to-create-a-reactive-google-map/
+    // and leaflet specific: http://asynchrotron.com/blog/2013/12/28/realtime-maps-with-meteor-and-leaflet-part-2/
+    var markers = [];
+    DailyBikeData.find({}).observe({
+      added: function(bike) {
+        var latlng = [bike.Positions.lat, bike.Positions.lng];
+        var marker = L.marker(latlng,
+            {title: "#" + bike.Bike + " is " + bike.Tag,
+            opacity: 0.5} // Adjust the opacity
+            ).addTo(map);
+        // marker.bindPopup("#" + bike.Bike + " is " + bike.Tag);
+
+        // Store this marker instance within the markers object.
+        markers[bike._id] = marker;
+
+        console.log(markers[bike._id] + ' added to map on ADDED event');
+      },
+      changed: function(bike, oldDocument) {
+        var latlng = [bike.Positions.lat, bike.Positions.lng];
+        markers[bike._id].setLatLng(latlng).update();
+
+        console.log(markers[bike._id] + ' changed on map on CHANGED event');
+      },
+      removed: function(oldBike) {
+        console.log(oldBike);
+        // Remove the marker from the map
+        map.removeLayer(markers[oldBike._id]);
+
+        // Remove the reference to this marker instance
+        delete markers[oldBike._id];
+
+        console.log(markers[oldBike._id] + ' removed from map on REMOVED event');
+      }
+    });
 
     var bottomLng = -76.936569; var topLng = -76.950603;
     var leftLat = 38.994052; var rightLat = 38.981376;
@@ -129,8 +115,9 @@ if (Meteor.isClient) {
       [leftLat, bottomLng]
     ]).addTo(map);
 
-    // Zoom to user location
-    map.locate({ setView: true })
+    // // Zoom to user location
+    // map.locate({ setView: true })
+    map.setView(new L.LatLng(38.987701, -76.940989), 13);
 
     // Notes for using included MarkCluster Package
     // var markers = new L.MarkerClusterGroup();

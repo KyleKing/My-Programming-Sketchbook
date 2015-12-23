8f4c80d599dda9c5e67b2b5f34c52075819358b5
Kyle King <kmking72@gmail.com>
2015-12-22 21:40:17 -0500

    Iterate over multiple dropbox files & Balloon.io
    Note: make sure delete function...doesn't uh...delete everything from Dropbox

--- Raspberry_Pi/Aloo/node/PhotoFrame.js
+++ Raspberry_Pi/Aloo/node/PhotoFrame.js
@@ -1,5 +1,6 @@
 // Global Variables to prevent overuse of API's:
-var download = 1;
+var download = 0
+var raspberry_pi = 0
 
 // Other globals
 var CollectionPhotos = []
@@ -51,7 +52,9 @@ function FetchPhotos() {
 				// console.log('url_o = ' + CollectionPhotos[0].url_o)
 				// console.log('id = ' + CollectionPhotos[0].id)
 				// console.log('originalformat = ' + CollectionPhotos[0].originalformat)
-				CompleteDownload(true)
+				if (download) {
+					CompleteDownload(true)
+				}
 			}
 		})
 	})
@@ -94,21 +97,29 @@ var client = app.client(SecretOptions.Dropbox_Token)
 var options = {
   root: "dropbox" // optional - defaults to "Sandbox" - i.e. "app folder"
 }
-var dropboxdir = 'Public/AlooPhotos'
+// var dropboxdir = 'Public/AlooPhotos'
+var dropboxdir = 'Apps/Balloon.io/alloo'
 client.metadata(dropboxdir, options, function(status, reply){
-	// console.log(reply.contents[0])
-  var filepath = reply.contents[0].path
-  console.log(filepath)
-	client.get(filepath, options, function(status, reply, metadata){
-  	var filename = path.basename(filepath)
-		var localpath = 'imgs/dropbox_' + filename
-		var wstream = fs.createWriteStream(localpath)
-		wstream.write(reply)
-		wstream.end(function () { console.log('done') })
+	for (var i = 0; i < reply.contents.length; i++) {
+		// console.log(reply.contents[0])
+	  var filepath = reply.contents[i].path
+	  console.log(filepath)
+		client.get(filepath, options, function(status, reply, metadata){
+			// Grab fresh file path because this is async
+			var filepath = metadata.path
 
-		// Yeah...don't do thie for a binary file...
-	  // console.log(reply.toString(), metadata)
-	})
+			var filename = path.basename(filepath)
+			console.log(filename)
+			var localpath = 'imgs/dropbox_' + filename
+			console.log(localpath);
+			var wstream = fs.createWriteStream(localpath)
+			wstream.write(reply)
+			wstream.end(function () { console.log('done') })
+
+			// Yeah...don't do thie for a binary file...
+		  // console.log(reply.toString(), metadata)
+		})
+	}
 })
 
 //
@@ -129,158 +140,182 @@ glob("imgs/*.*", function (er, files) {
 	// console.log(files);
 })
 
-//
-//
-// Part 3: Download [Flickr] Photos
-//
-//
-
-// Code Source: http://stackoverflow.com/a/12751657/3219667
-var fs = require('fs'),
-		request = require('request')
-
-function DownloadURL(uri, filename, callback) {
-	if (fs.existsSync(filename)) {
-		console.log(filename + ' already exists')
-	} else {
-		request.head(uri, function(err, res, body){
-			// console.log('content-type:', res.headers['content-type'])
-			// console.log('content-length:', res.headers['content-length'])
-
-			request(uri).pipe(fs.createWriteStream(filename)).on('close', callback)
-		})
-		console.log('Download: ' + filename)
-	}
-}
-
-function DeleteExcessFiles() {
-	if ( (ExistingFiles.length - DesiredFiles.length) === 0) {
-		console.log('Nothing to report')
-	} else {
-		for (var i = 0; i < ExistingFiles.length; i++) {
-			var filename = ExistingFiles[i]
-			if (DesiredFiles.indexOf(filename) === -1) {
-				// Delete File
-				fs.unlink(filename, function(err) {
-						if (err) throw err
-				});
-			}
-		}
-	}
-}
-
-function CompleteDownload(status) {
-	for (var i = 0; i < CollectionPhotos.length; i++) {
-		var filename = 'imgs/' + CollectionPhotos[i].source + '_' + CollectionPhotos[i]._id + '.' + CollectionPhotos[i].type
-		var url = CollectionPhotos[i].url
-		if (download === 0) {
-			console.log('Downloads turned off');
-		} else if (url === undefined || filename === undefined || CollectionPhotos[i].type === undefined) {
-			console.log('---------failed-----------')
-			console.log(CollectionPhotos[i])
-		} else {
-			// Store properly formatted files
-			DesiredFiles.push(filename)
-			DownloadURL( url, filename, function(err) {
-					if (err) throw err
-			})
-		}
-		// // End the program
-		// End(true)
-	}
-	DeleteExcessFiles()
-	MakePictures()
-}
-
-
 // //
 // //
-// // For ending the program
+// // Part 3: Download [Flickr] Photos
 // //
 // //
-// function End(status) {
-//  // Stop code after completeDownload
-//  if (status) process.exit()
+
+// // Code Source: http://stackoverflow.com/a/12751657/3219667
+// var fs = require('fs'),
+// 		request = require('request')
+
+// if (download) {
+// 	function DownloadURL(uri, filename, callback) {
+// 		if (fs.existsSync(filename)) {
+// 			console.log(filename + ' already exists')
+// 		} else {
+// 			request.head(uri, function(err, res, body){
+// 				// console.log('content-type:', res.headers['content-type'])
+// 				// console.log('content-length:', res.headers['content-length'])
+
+// 				request(uri).pipe(fs.createWriteStream(filename)).on('close', callback)
+// 			})
+// 			console.log('Download: ' + filename)
+// 		}
+// 	}
+
+// 	function DeleteExcessFiles() {
+// 		if ( (ExistingFiles.length - DesiredFiles.length) === 0) {
+// 			console.log('Nothing to report')
+// 		} else {
+// 			for (var i = 0; i < ExistingFiles.length; i++) {
+// 				var filename = ExistingFiles[i]
+// 				if (DesiredFiles.indexOf(filename) === -1) {
+// 					// Delete File
+// 					fs.unlink(filename, function(err) {
+// 							if (err) throw err
+// 					});
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	function CompleteDownload(status) {
+// 		for (var i = 0; i < CollectionPhotos.length; i++) {
+// 			var filename = 'imgs/' + CollectionPhotos[i].source + '_' + CollectionPhotos[i]._id + '.' + CollectionPhotos[i].type
+// 			var url = CollectionPhotos[i].url
+// 			if (download === 0) {
+// 				console.log('Downloads turned off');
+// 			} else if (url === undefined || filename === undefined || CollectionPhotos[i].type === undefined) {
+// 				console.log('---------failed-----------')
+// 				console.log(CollectionPhotos[i])
+// 			} else {
+// 				// Store properly formatted files
+// 				DesiredFiles.push(filename)
+// 				DownloadURL( url, filename, function(err) {
+// 						if (err) throw err
+// 				})
+// 			}
+// 			// // End the program
+// 			// End(true)
+// 		}
+// 		DeleteExcessFiles()
+// 		if (raspberry_pi) {
+// 			MakePictures()
+// 		}
+// 	}
 // }
 
 
-//
-//
-// Step 4: Schedule this task to run once a day
-//
-//
-var CronJob = require('cron').CronJob
-// var job = new CronJob('00 30 11 * * 1-5', function() {
-	/*
-	 * Runs every weekday (Monday through Friday)
-	 * at 11:30:00 AM. It does not run on Saturday
-	 * or Sunday.
-	 */
-
-// Quick CRON guide
-// second         0-59
-// minute         0-59
-// hour           0-23
-// day of month   0-31
-// month          0-12 (or names, see below)
-// day of week    0-7
-
-var Fetch = new CronJob('00 00 9 * * *', function() {
-  FetchPhotos()
-  }, function () {
-    /* This function is executed when the job stops */
-    console.log('Finished')
-  },
-  false /* Start the job right now */
-)
 
 
-FetchPhotos()
-Fetch.start()
+// // //
+// // // For ending the program
+// // //
+// // function End(status) {
+// //  // Stop code after completeDownload
+// //  if (status) process.exit()
+// // }
 
 
-//
-// Step 4.2: Create slide show
-//
-// Allow for Scripting
-var exec = require('child_process').exec;
-
-function MakePictures() {
-	// console.log(DesiredFiles[0]);
-	var RandIndex = Math.round( DesiredFiles.length*Math.random() )
-	var filepath = DesiredFiles[RandIndex]
-	// sudo fbi -a -noverbose -T 10 /home/pi/Raspberry\ Pi/Aloo/imgs/6.png
-	var command = 'sudo fbi -a -noverbose -T 10 /home/pi/Raspberry_Pi/Aloo/node/' + filepath
-	// console.log(command)
-	var child = exec(command, function (error, stdout, stderr) {
-		if (error) console.log(error)
-		console.log('stdout: ' + stdout)
-		console.log('stderr: ' + stderr)
-	})
-}
-
-// Refresh image every minute
-var SlideShow = new CronJob('00 * * * * *', function() {
-		MakePictures()
-	}, function () {
-    /* This function is executed when the job stops */
-    console.log('Finished')
-  },
-  true /* Start the job right now */
-)
 
 
-// // Source: http://stackoverflow.com/a/3510850/3219667
-// // kill $(ps aux | grep 'fbi' | awk '{print $2}')
-// // ps -ef | grep fbi
+// //
+// //
+// // Step 4: Schedule this task to run once a day
+// //
+// //
+// var CronJob = require('cron').CronJob
+// // var job = new CronJob('00 30 11 * * 1-5', function() {
+// 	/*
+// 	 * Runs every weekday (Monday through Friday)
+// 	 * at 11:30:00 AM. It does not run on Saturday
+// 	 * or Sunday.
+// 	 */
 
-// // Then Kill image viewer
-// setTimeout(function() {
-// 	var command = 'sudo pkill fbi'
-// 	console.log(command)
-// 	var KillProcess = exec(command, function (error, stdout, stderr) {
-// 		if (error) console.log(error)
-// 		console.log('stdout: ' + stdout)
-// 		console.log('stderr: ' + stderr)
-// 	})
-// }, 5000)
+// // Quick CRON guide
+// // second         0-59
+// // minute         0-59
+// // hour           0-23
+// // day of month   0-31
+// // month          0-12 (or names, see below)
+// // day of week    0-7
+
+// var Fetch = new CronJob('00 00 9 * * *', function() {
+//   FetchPhotos()
+//   }, function () {
+//     /* This function is executed when the job stops */
+//     console.log('Finished')
+//   },
+//   false /* Start the job right now */
+// )
+
+
+// FetchPhotos()
+// Fetch.start()
+
+
+// // Run only on raspberry pi
+// if (raspberry_pi) {
+// 	//
+// 	// Step 4.2: Create slide show
+// 	//
+// 	// Allow for Scripting
+// 	var exec = require('child_process').exec;
+
+// 	function MakePictures() {
+// 		// console.log(DesiredFiles[0]);
+// 		var RandIndex = Math.round( DesiredFiles.length*Math.random() )
+// 		var filepath = DesiredFiles[RandIndex]
+// 		// sudo fbi -a -noverbose -T 10 /home/pi/Raspberry\ Pi/Aloo/imgs/6.png
+// 		var command = 'sudo fbi -a -noverbose -T 10 /home/pi/Raspberry_Pi/Aloo/node/' + filepath
+// 		// console.log(command)
+// 		var child = exec(command, function (error, stdout, stderr) {
+// 			if (error) console.log(error)
+// 			console.log('stdout: ' + stdout)
+// 			console.log('stderr: ' + stderr)
+// 		})
+// 	}
+
+// 	// Refresh image every minute
+// 	var SlideShow = new CronJob('00 * * * * *', function() {
+// 			MakePictures()
+// 		}, function () {
+// 	    /* This function is executed when the job stops */
+// 	    console.log('Finished')
+// 	  },
+// 	  true /* Start the job right now */
+// 	)
+
+
+// 	// // Source: http://stackoverflow.com/a/3510850/3219667
+// 	// // kill $(ps aux | grep 'fbi' | awk '{print $2}')
+// 	// // ps -ef | grep fbi
+
+// 	// // Then Kill image viewer
+// 	// setTimeout(function() {
+// 	// 	var command = 'sudo pkill fbi'
+// 	// 	console.log(command)
+// 	// 	var KillProcess = exec(command, function (error, stdout, stderr) {
+// 	// 		if (error) console.log(error)
+// 	// 		console.log('stdout: ' + stdout)
+// 	// 		console.log('stderr: ' + stderr)
+// 	// 	})
+// 	// }, 5000)
+// }
+
+// //
+// // Not implemented
+// //
+// // USB
+// // If Dropbox is a bust, consider triggering an event to read a USB drive
+// // Two possible NPM packages:
+// // 	https://www.npmjs.com/package/usb-detection
+// // 	https://github.com/nonolith/node-usb
+// // Then display a pre-made photo that says, drive can be ejected now. Then when drive is ejected, revert to the usual slideshow
+// //
+// // Power-Saving
+// // Notes are in a separate file (init.js), if real-world sensor, can turn display on/off
+// // Additionally, the power button could be hardwired to the pi and controlled via GPIO
+

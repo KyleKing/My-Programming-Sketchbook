41d0ffd2097fc32d8401b991c755a5b0bc16f3b0
Kyle King <kmking72@gmail.com>
2016-01-26 12:32:02 -0500

    Working search and sort demo

--- Meteorjs/Meteor Tabular Examples/Minimal/Minimal.js
+++ Meteorjs/Meteor Tabular Examples/Minimal/Minimal.js
@@ -1,10 +1,8 @@
-// Would otherwise be in the lib folder:
-var sales = new Mongo.Collection('sales');
+// Could be in lib/collections.js
+sales = new Mongo.Collection('sales');
 
-if (Meteor.isServer && sales.find().count() === 0) {
-  sales.insert({date: 1, quantity: 2, sales: 3, discount: 4, profit: 12, region: 15, segment: 16, category: 1});
-}
-
+// Declared on both client and server, so could be in a folder called 'both'
+// But not in the client or server folders
 TabularTables = {};
 Meteor.isClient && Template.registerHelper('TabularTables', TabularTables);
 TabularTables.sales = new Tabular.Table({
@@ -14,129 +12,38 @@ TabularTables.sales = new Tabular.Table({
     "dom": 'firt<"bottom"lp><"clear">',
     "lengthMenu": [5, 10, 25, 50],
     columns: [
-      { data: "date", title: "date", class: "my_col", width: "12%" },
-      { data: "quantity", title: "quantity", class: "my_col", width: "12%" },
-      { data: "sales", title: "sales", class: "my_col", width: "11%" },
-      { data: "discount", title: "discount", class: "my_col", width: "11%" },
-      { data: "profit", title: "profit", class: "my_col", width: "11%" },
-      { data: "region", title: "region", class: "my_col", width: "11%" },
-      { data: "segment", title: "segment", class: "my_col", width: "12%" },
-      { data: "category", title: "category", class: "my_col", width: "12%" },
-      { tmpl: Meteor.isClient && Template.SalesActionBtns, title: "Edit/Delete", width: "8%"}
+      { data: "date", title: "date", class: "date"},
+      { data: "quantity", title: "quantity", class: "quantity"},
+      { data: "sales", title: "sales", class: "sales"},
+      { data: "discount", title: "discount", class: "discount"},
+      { data: "profit", title: "profit", class: "profit"},
+      { data: "region", title: "region", class: "region"},
+      { data: "segment", title: "segment", class: "segment"},
+      { data: "category", title: "category", class: "category"},
+      { tmpl: Meteor.isClient && Template.SalesActionBtns, title: "Edit/Delete"}
     ]
 });
 
+// Could be somewhere in the client folder:
 if (Meteor.isClient) {
-    //Client:
-    Template.TableContainer.created = function() {
-        Meteor.subscribe("singlesale", Session.get('salesID'));
-    }
-    var Troubleshoot;
-    Troubleshoot = null;
-    this.TabularSelectorInit = function(TableID) {
-        var sel;
-        if (typeof window.TabularSelector === 'undefined') {
-            window.TabularSelector = new ReactiveVar({});
-        }
-        sel = window.TabularSelector.get();
-        sel[TableID] = {};
-        sel[TableID].TheseClasses = [];
-        return window.TabularSelector.set(sel);
-    };
-    this.TabularSelectorMain = function(TableID, collection) {
-        var SelectedTable;
-        SelectedTable = '#' + TableID + ' thead th';
-        return $(SelectedTable).each(function() {
-            var $input, ColTitle, ThisClass, htmlSnippet, sel;
-            sel = window.TabularSelector.get()[TableID];
-            ColTitle = $(SelectedTable).eq($(this).index()).text();
-            ThisClass = $(SelectedTable).eq($(this).index()).attr(
-                'class');
-            ThisClass = ThisClass.replace(/(sortin)\w+/gi, '').trim();
-            if (typeof(_.findWhere(sel, ThisClass)) === 'undefined') {
-                sel.TheseClasses.push(ThisClass);
-            }
-            if (!(typeof ThisClass === 'undefined' || ThisClass ===
-                '')) {
-                htmlSnippet =
-                    '<input type="text" placeholder="Search ';
-                $input = $(htmlSnippet + ColTitle + '"' + 'class="' +
-                    ThisClass + '"/>');
-                $(this).html($input);
-                $input.on('click', function(e) {
-                    return e.stopPropagation();
-                });
-                return $input.on('keyup', function(e) {
-                    var GlobalSel, SearchString, column,
-                        columns;
-                    SearchString = this.value;
-                    sel = window.TabularSelector.get()[
-                        TableID];
-                    sel[ThisClass] = {};
-                    if (SearchString) {
-                        columns = TabularTables[collection]
-                            .options.columns;
-                        column = _.clone(_.filter(columns,
-                            function(item, index) {
-                                return item.title ===
-                                    ColTitle;
-                            }));
-                        sel[ThisClass].value = Util.getPubSelector({},
-                            SearchString, {}, true,
-                            column);
-                    } else {
-                        delete sel[ThisClass];
-                    }
-                    GlobalSel = window.TabularSelector.get();
-                    GlobalSel[TableID] = sel;
-                    if (Troubleshoot === TableID) {
-                        console.log(
-                            'window.TabularSelector from, ' +
-                            TableID + ':');
-                        console.log(GlobalSel);
-                    }
-                    return window.TabularSelector.set(
-                        GlobalSel);
-                });
-            }
-        });
-    };
-    this.TabularSelectorHelper = function(TableID) {
-        var ReactiveTest, sel;
-        sel = window.TabularSelector.get();
-        sel = sel[TableID];
-        ReactiveTest = {};
-        ReactiveTest['$and'] = [{}];
-        _.each(sel.TheseClasses, function(ThisClass) {
-            if (typeof sel[ThisClass] !== 'undefined') {
-                if (typeof sel[ThisClass].value !== 'undefined') {
-                    return ReactiveTest['$and'].push(sel[ThisClass]
-                        .value);
-                }
-            }
-        });
-        if (Troubleshoot === TableID) {
-            console.log('ReactiveTest Variable from ' + TableID +
-                ' Helper Function:');
-            console.log(ReactiveTest);
-        }
-        return ReactiveTest;
-    };
-    Template.TableContainer.created = function() {
-        return TabularSelectorInit('salesdata');
-    };
-    Template.TableContainer.rendered = function() {
-        return TabularSelectorMain('salesdata', 'sales');
-    };
-    Template.TableContainer.helpers({
-        currentSelector: function() {
-            return TabularSelectorHelper('salesdata');
-        }
-    });
+	Template.TableContainer.created = function() {
+	    return TabularSelectorInit('salesdata');
+	};
+	Template.TableContainer.rendered = function() {
+	    return TabularSelectorMain('salesdata', 'sales');
+	};
+	Template.TableContainer.helpers({
+	    currentSelector: function() {
+	        return TabularSelectorHelper('salesdata');
+	    }
+	});
 }
 
-if (Meteor.isServer) {
-    Meteor.startup(function() {
-        // code to run on server at startup
-    });
+
+// Could be in server/seed_data.js
+if (Meteor.isServer && sales.find().count() === 0) {
+  sales.insert({date: 'test 1', quantity: 2, sales: 3, discount: 4, profit: 12, region: 15, segment: 16, category: 1});
+  sales.insert({date: 'test 3', quantity: 3, sales: 3, discount: 4, profit: 12, region: 15, segment: 16, category: 1});
+  sales.insert({date: 'test 4', quantity: 4, sales: 3, discount: 4, profit: 12, region: 15, segment: 16, category: 1});
+  sales.insert({date: 'test 2', quantity: 5, sales: 3, discount: 4, profit: 12, region: 15, segment: 16, category: 1});
 }

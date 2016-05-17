66b30c93c8f1509ae475450deadf554a585465f6
Kyle King <kmking72@gmail.com>
2016-05-17 12:10:09 -0400

    Cleaned up and output now in "compiled" dir

--- AppleScripts/compile.bash
+++ AppleScripts/compile.bash
@@ -6,15 +6,24 @@ while IFS= read -r -d $'\0' file; do
   dirname="${file%/*}/"
   basename="${file:${#dirname}}"
   # echo mv "$file" "$dirname${basename%.*}_$basename"
+
   # # Modify only .applescript files
   # echo ${basename: -12} # get last 12 characters
   if [[ ${basename: -12} == ".applescript" ]]; then
+
+	  # Make sure compiled directory exists
+	  compiledDir="$dirname""compiled/"
+	  if [ ! -d "$compiledDir" ]; then
+	  	mkdir $compiledDir
+	  fi
+
 	  oldName="$dirname$basename"
-	  newName="$dirname${basename%.applescript*}.scpt"
+	  newName="$dirname""compiled/${basename%.applescript*}.scpt"
 	  # echo $oldName
 	  # echo $newName
 	  # # Compile .applescript file found above
 	  echo Compiling ${basename%.applescript*}
 	  osacompile -o $newName $oldName
+
   fi
 done < <(find . -type f -print0)

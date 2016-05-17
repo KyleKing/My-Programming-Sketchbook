c1e47912d1e0d6b66b9026bb36751cf75424602c
Kyle King <kmking72@gmail.com>
2016-05-17 11:59:46 -0400

    Fixed compile script, run when needed

--- Scripts/compile.bash
+++ Scripts/compile.bash
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # Modified from: http://stackoverflow.com/a/1957258/3219667
+# Recursive compile of all .applescript files
 while IFS= read -r -d $'\0' file; do
-	# echo *.applescript
   dirname="${file%/*}/"
   basename="${file:${#dirname}}"
   # echo mv "$file" "$dirname${basename%.*}_$basename"
+  # # Modify only .applescript files
   # echo ${basename: -12} # get last 12 characters
   if [[ ${basename: -12} == ".applescript" ]]; then
 	  oldName="$dirname$basename"
-	  echo $oldName
 	  newName="$dirname${basename%.applescript*}.scpt"
-	  echo $newName
-	  echo osacompile -o $newName $oldName
+	  # echo $oldName
+	  # echo $newName
+	  # # Compile .applescript file found above
+	  echo Compiling ${basename%.applescript*}
+	  osacompile -o $newName $oldName
   fi
-	echo -----
 done < <(find . -type f -print0)

b1eba641449a249f34cc8a5b80157033b899c399
Kyle King <kmking72@gmail.com>
2016-05-27 12:14:53 -0400

    Just went and built a DDP: Node-Meteor app set...

---
+++ JavaScript/Minimal_python_ddp/install.sh
@@ -0,0 +1,8 @@
+# Install browser-refresh package globally
+npm install -g browser-refresh;
+
+# Install all of the local node modules
+npm install;
+
+# Run index.js (Uses the "main" property of package.json)
+browser-refresh;
---
+++ JavaScript/Minimal_python_ddp/process.bash
@@ -0,0 +1,13 @@
+# Run a meteor Application
+cd /Users/kyleking/Developer/My-Programming-Sketchbook/JavaScript/minimal/; meteor
+
+# # Repeat sending a command to Hammerspoon
+# # Source: http://stackoverflow.com/a/17350229/3219667
+# for x in {1..20} ; do
+# 	# # If you have Hammerspoon installed:
+# 	# /usr/local/bin/hs -c "AlertUser('STILL RUNNING!')"
+# 	# More universal: make the computer repeatably beep
+# 	echo $x
+# 	# tput bel
+# 	sleep 1
+# done

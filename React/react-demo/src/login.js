82a92857c47eaa48f88d1bb06c8845b07db155a4
Kyle King <kmking72@gmail.com>
2018-11-06 21:19:53 -0500

    Refac: login screen & Add: brcyptjs demo

--- React/react-demo/src/login.js
+++ React/react-demo/src/login.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import bcrypt from 'bcryptjs'
 import './Login.css'
 
+// TODO: Move to nedDB (Store `hash` in your DB)
+var basicPass = 'hello_world'  // FIXME: Store password only as hash
+var hash = bcrypt.hashSync( basicPass, bcrypt.genSaltSync( 15 ) )
+
+
 class Login extends Component {
+	// Login input text
+
 	constructor( props ) {
 		super( props )
-		this.state = { password: '', hidePassword: true }
+		this.state = { loadState: false, password: '', hidePass: false }
 		// Bind handlers
 		this.showHide = this.showHide.bind( this )
 		this.handleChange = this.handleChange.bind( this )
@@ -15,7 +23,7 @@ class Login extends Component {
 		e.preventDefault()
 		e.stopPropagation()
 		// Toggle showing/hiding the password input
-		this.setState({ hidePassword: !this.state.hidePassword })
+		this.setState({ hidePass: !this.state.hidePass })
 	}
 
 	handleChange( e ) {
@@ -25,29 +33,50 @@ class Login extends Component {
 
 	handleSubmit( e ) {
 		e.preventDefault()
-		// TODO: Check authentication and route to Alarms dashboard
-		alert( `Submitted Password: ${this.state.password}` )
+		// Hide password and disable form input while loading
+		this.setState({hidePass: true, loadState: true})
+		bcrypt.compare( this.state.password, hash ).then( ( res ) => {
+			if ( res ) {
+				console.log( 'Update route' )
+			} else {
+				console.log( this.state.password )
+			}
+		} ).catch( ( err ) => {
+			alert( err )
+		} ).finally( () => {
+			this.setState({loadState: false})
+		})
 	}
 
 	render() {
 		// <Link to="/">Home</Link>
-		const toggle_label = this.state.hidePassword ? 'Hide' : 'Show'
+		const toggle_label = this.state.hidePass ? 'Hide' : 'Show'
 		return (
 			<div className="center-up">
 				<h1>PiAlarm</h1>
 				<form onSubmit={this.handleSubmit}>
-					<div className="password-group">
+					<div className="pass-container">
 						<input
 							className={`password ${toggle_label} dark`}
-							type={this.state.hidePassword ? 'password' : 'text'}
+							type={this.state.hidePass ? 'password' : 'text'}
 							onChange={this.handleChange}
 							placeholder="Password"
 							value={this.state.password}
+							required={true}
+							disabled={this.state.loadState}
 						/>
-						<button
-							className="password-toggle"
-							onClick={this.showHide}
-						>{toggle_label}</button>
+						<div className="pass-container-buttons">
+							<button
+								className="password-toggle"
+								onClick={this.showHide}
+								type="button"
+							>{toggle_label}</button>
+							<button
+								type="submit"
+								className="login"
+								disabled={this.state.loadState}
+							>Login</button>
+						</div>
 					</div>
 				</form>
 			</div>

b5606a69c55c8fd6f8c26658e1fe55a4672f9471
Kyle King <KyleKing@users.noreply.github.com>
2020-12-06 15:33:09 -0500

    New: add snippet for cmd2 BorderedTable

--- Assorted_Snippets/python/cli_prototyping/proto_04.py
+++ Assorted_Snippets/python/cli_prototyping/proto_04.py
@@ -1,8 +1,11 @@
 """Experimenting with cmd2 modular command sets."""
 
 import argparse
+from functools import partial
+from typing import List
 
 from cmd2 import Cmd, Cmd2ArgumentParser, CommandSet, Statement, with_argparser, with_category, with_default_category
+from cmd2.table_creator import BorderedTable, Column
 
 
 @with_default_category('Fruits')
@@ -30,9 +33,7 @@ class LoadableVegetables(CommandSet):
 
 
 class ExampleApp(Cmd):
-    """
-    CommandSets are loaded via the `load` and `unload` commands
-    """
+    """CommandSets are loaded via the `load` and `unload` commands."""
 
     def __init__(self, *args, **kwargs):
         # gotta have this or neither the plugin or cmd2 will initialize
@@ -70,6 +71,45 @@ class ExampleApp(Cmd):
         if ns.cmds == 'vegetables':
             self.unregister_command_set(self._vegetables)
             self.poutput('Vegetables unloaded')
+
+
+# ======================================================================================================
+# PLANNED: This is just a cool looking table output that may be useful
+def create_cmd_table(table_data: List[List[str]], width: int = 15) -> BorderedTable:
+    """Create a bordered table for cmd2 output.
+
+    Args:
+        table_data: list of lists with the string data to display
+        width: integer width of the columns. Default is 15 which generally works for ~4 columns
+
+    Returns:
+        BorderedTable: generated table for printing
+
+    """
+    columns = table_data[0]
+    auto_column = partial(Column, width=width)
+    bt = BorderedTable([*map(auto_column, columns)])
+    rows = table_data[1:]
+    return bt.generate_table(rows)
+
+
+class ExtendedDisplay:  # noqa: H601
+    """Extended Display Command Set with New Commands for More Interactive Output."""
+
+    def do_display_table(self, statement: Statement) -> None:
+        """Display a Table of (TBD) Data.
+
+        Args:
+            statement: results of parsing
+
+        """
+        # Placeholder sample data
+        _table_data = [
+            ['Studio', '# Movies', '# PPV', 'Revenue'],
+            ['Netflix', '12', '14', f'{999999:,}'],
+            ['Amazon Prime Video', '12', '14', f'{21450:,}'],
+        ]
+        self._cmd.poutput(create_cmd_table(_table_data))
 
 
 if __name__ == '__main__':
